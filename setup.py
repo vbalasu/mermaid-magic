2c0142195d9f3a12e70bc11030e120cdad92b735
Vijay Balasubramaniam <vbalasu@gmail.com>
2025-05-03 08:47:56 -0700

    Incremented version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mermaid-magic",
-    version="0.1.0",
+    version="0.1.1",
     description="IPython magic for rendering Mermaid diagrams",
     long_description=long_description,
     long_description_content_type="text/markdown",
