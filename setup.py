df3bc6e9805b51087feb0483cedcd5de4925a9e6
Vijay Balasubramaniam <vbalasu@gmail.com>
2025-05-03 21:08:50 -0700

    PyPI version 0.1.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mermaid-magic",
-    version="0.1.3",
+    version="0.1.4",
     description="IPython magic for rendering Mermaid diagrams",
     long_description=long_description,
     long_description_content_type="text/markdown",
